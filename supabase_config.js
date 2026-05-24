43b9a1a87a7882baa1caf147efb0ab975019b14d
Rushikesh Raut <rushig2137@gmail.com>
2026-05-24 10:53:57 +0530

    Add files via upload
    google authentication

--- supabase_config.js
+++ supabase_config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
-  url: 'YOUR_SUPABASE_PROJECT_URL',
-  anonKey: 'YOUR_SUPABASE_ANON_KEY'
+  url: 'https://grfvuspjxqfsoaqvdyrl.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdyZnZ1c3BqeHFmc29hcXZkeXJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkwNDQxNTEsImV4cCI6MjA5NDYyMDE1MX0.6f2pKR1K9xCS_xm5cGkToVtQ6dz6QrLdu90SCqmqm3M'
 };
